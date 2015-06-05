fb0860ff8a1f01d107310380e948ea93af4f3091
Emirrah Hazelle Dogelio <em.dogelio@teamcodeflux.com>
2015-06-05 12:48:41 +0800

    Move mGoogleApiClient related codes to ShowResultsActivity
    Signed-off-by: Emirrah Hazelle Dogelio <em.dogelio@teamcodeflux.com>

--- src/main/java/com/teamcodeflux/android/training/app/ShowResultsActivity.java
+++ src/main/java/com/teamcodeflux/android/training/app/ShowResultsActivity.java
@@ -1,14 +1,18 @@
 package com.teamcodeflux.android.training.app;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.widget.TextView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import org.androidannotations.annotations.*;
+
 
 /**
  * Created by em.dogelio on 5/29/15.
  */
 @EActivity(R.layout.activity_show_results)
-public class ShowResultsActivity extends Activity {
+public class ShowResultsActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     @ViewById(R.id.results_list)
     TextView results;
@@ -19,5 +23,46 @@ public class ShowResultsActivity extends Activity {
     @AfterViews
     void displayResults() {
         results.setText(keyword);
+    }
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
